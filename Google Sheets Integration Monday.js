bc4d25a1d1a7f0cd5a75a01ad6998edbae14ae86
Marcos Alexandre <117322129+MarcosQB@users.noreply.github.com>
2022-11-26 16:42:15 -0300

    Add files via upload

---
+++ Google Sheets Integration Monday.js
@@ -0,0 +1,52 @@
+function sellersGeral() {
+
+  // Pegando a planilha
+  var planilhaAtiva = SpreadsheetApp.getActiveSpreadsheet(); 
+  var aba = planilhaAtiva.getSheetByName("SELLERS");
+  var cont = 0
+
+  // Requisicao para a API
+  var base_url = "https://api.monday.com/v2";
+  var query = `query {
+  boards (ids: 2283782997) {
+    items {
+      name
+      }
+    }
+  }
+`
+  var headers = {
+    "Authorization": Autorizaes.autorizacoes.monday,
+    "Content-Type": "application/json"
+
+  }
+
+  var options = {
+    headers: headers,
+    method: "POST",
+    payload: JSON.stringify({
+      query: query
+    })
+  }
+
+  var response = UrlFetchApp.fetch(base_url, options).getContentText();
+  var result = JSON.parse(response);
+
+  // Formatando o retorno da API
+  var retorno = result.data.boards;
+
+ for (let i = 0 ; i < retorno.length; i++ ){
+  var items = retorno[i].items
+   for(let j = 0; j < items.length; j++){
+     cont++
+    var names = items[j].name
+
+      // Salvando os dados na planilha.
+       aba.getRange(1+cont,1).setValue(names);
+       let maisDois = (cont+2)
+       var range = "A"+maisDois+":A"+maisDois;
+
+   }
+  }
+        aba.getRange(range).setValue("");
+}

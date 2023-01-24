3ec5b70eda7bb393295e94ba44aab2a857599f20
Marcos Alexandre <117322129+MarcosQB@users.noreply.github.com>
2023-01-24 15:50:26 -0300

    Update Google Sheets Integration Monday.js

--- Google Sheets Integration Monday.js
+++ Google Sheets Integration Monday.js
@@ -6,55 +6,36 @@ O Script abaixo foi construido na IDE do Google Apps Script
 segue a documentação:
 https://developers.google.com/apps-script
 
-function sellersGeral() {
-
-  // Pegando a planilha
-  var planilhaAtiva = SpreadsheetApp.getActiveSpreadsheet(); 
-  var aba = planilhaAtiva.getSheetByName("SELLERS");
-  var cont = 0
-
-  // Requisicao para a API
-  var base_url = "https://api.monday.com/v2";
-  var query = `query {
-  boards (ids: 2283782997) {
-    items {
-      name
+function empresa(){
+  const baseUrl = "https://api.monday.com/v2";
+  const query = 
+        `query {
+    boards (ids: 3430011657) {
+      items {
+        name
       }
     }
-  }
-`
-  var headers = {
-    "Authorization": Autorizaes.autorizacoes.monday,
-    "Content-Type": "application/json"
-
-  }
-
-  var options = {
-    headers: headers,
+  }`;
+  
+  const headers = {
+    Authorization: Autorizaes.autorizacoes.monday,
+    "Content-Type": "application/json",
+  };
+  const options = {
+    headers,
     method: "POST",
     payload: JSON.stringify({
-      query: query
-    })
-  }
+      query,
+    }),
+  };
+  const response = UrlFetchApp.fetch(baseUrl, options).getContentText();
+  const { data } = JSON.parse(response);
+  const { items } = data.boards[0];
+  const names = items.map(({ name }) => name).flat();
 
-  var response = UrlFetchApp.fetch(base_url, options).getContentText();
-  var result = JSON.parse(response);
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const aba = ss.getSheetByName('Base');
 
-  // Formatando o retorno da API
-  var retorno = result.data.boards;
-
- for (let i = 0 ; i < retorno.length; i++ ){
-  var items = retorno[i].items
-   for(let j = 0; j < items.length; j++){
-     cont++
-    var names = items[j].name
-
-      // Salvando os dados na planilha.
-       aba.getRange(1+cont,1).setValue(names);
-       let maisDois = (cont+2)
-       var range = "A"+maisDois+":A"+maisDois;
-
-   }
-  }
-        aba.getRange(range).setValue("");
-}
+  aba.getRange('A2:A').clearContent();
+  aba.getRange('A2:A' + (names.length + 1)).setValues(names.map((name) => [name]))
+};
